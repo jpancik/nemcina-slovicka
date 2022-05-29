615520706970913abf1a0084c1d8d61727eaf345
Juraj Pancik <jpancik@gmail.com>
2022-05-29 17:36:28 +0200

    Added initial framework with lektion 04.

---
+++ main.js
@@ -0,0 +1,58 @@
+let onKeyUpCallback;
+window.onkeyup = function(e) {
+	if (onKeyUpCallback) {
+		onKeyUpCallback();
+	}
+}
+
+// https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
+function showCard(root, german, slovak) {
+	const row1 = document.createElement('div');
+	row1.classList.add('row');
+	const row2 = document.createElement('div');
+	row2.classList.add('row');
+	row2.append(slovak);
+
+	const card = document.createElement('div');
+	card.classList.add('card');
+	card.append(row1);
+	card.append(row2);
+
+	root.append(card);
+
+	return new Promise(resolve => {
+		let cardShown = false;
+		const callback = () => {
+			if (!cardShown) {
+				row1.append(german);
+				cardShown = true;
+			} else {
+				onKeyUpCallback = null;
+				resolve();
+			}
+		};
+		card.addEventListener('click', callback);
+		onKeyUpCallback = callback;
+	});
+}
+
+async function main() {
+	const cardsList = document.querySelector('.cards-list');
+	
+	for (const word of shuffle(listOfWords)) {
+		cardsList.innerHTML = '';
+		await showCard(cardsList, word.german, word.slovak);
+	}
+
+	cardsList.innerHTML = 'Finished.';
+}
+
+main();
